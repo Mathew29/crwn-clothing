5dfdbfecab3e1c9fa6feefe3c5b9ae90819b794d
Mathew <mwakre29@gmail.com>
2021-05-25 12:50:50 -0700

    Create MenuItem file

---
+++ src/components/menu-item/menu-item.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./menu-item.scss";
+
+const MenuItem = () => {
+  return (
+    <div className="menu-item">
+      <div className="content">
+        <h1 className="title">Hats</h1>
+        <span className="subtitle">Shop Now</span>
+      </div>
+    </div>
+  );
+};
+
+export default MenuItem;
