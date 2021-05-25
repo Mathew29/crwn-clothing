db626d4523c76835ed4a64828e72eceb55b1c26a
Mathew <mwakre29@gmail.com>
2021-05-25 15:14:26 -0700

    dynamically render directory with style

--- src/components/menu-item/menu-item.jsx
+++ src/components/menu-item/menu-item.jsx
@@ -2,11 +2,16 @@ import React from "react";
 
 import "./menu-item.scss";
 
-const MenuItem = () => {
+const MenuItem = ({ title, imageUrl, size }) => {
   return (
-    <div className="menu-item">
+    <div
+      style={{
+        backgroundImage: `url(${imageUrl})`,
+      }}
+      className={`${size} menu-item`}
+    >
       <div className="content">
-        <h1 className="title">Hats</h1>
+        <h1 className="title">{title}</h1>
         <span className="subtitle">Shop Now</span>
       </div>
     </div>
