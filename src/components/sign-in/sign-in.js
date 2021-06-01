c8e3b00176abb67c9b325584a93c1512739106fe
Mathew <mwakre29@gmail.com>
2021-06-01 14:40:20 -0700

    create sign in form

---
+++ src/components/sign-in/sign-in.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+
+import "./sign-in.scss";
+
+class SignIn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({ email: "", password: "" });
+  };
+
+  handleChange = (event) => {
+    const { value, name } = event.target;
+
+    //dynamically set state
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    return (
+      <div className="sign-in">
+        <h2>I already have an account</h2>
+        <span>Sign in with you email and password</span>
+
+        <form onSubmit={this.handleSubmit}>
+          <input
+            name="email"
+            type="email"
+            value={this.state.email}
+            onChange={this.handleChange}
+            required
+          />
+          <label>Email</label>
+          <input
+            name="password"
+            type="password"
+            value={this.state.password}
+            onChange={this.handleChange}
+            required
+          />
+          <label>Password</label>
+
+          <input type="submit" value="Submit Form" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignIn;
